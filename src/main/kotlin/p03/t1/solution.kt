f8dbe00a277a4018b90a1c52c6d443d0f1f56a0c
Catzoy <demchenko20021@gmail.com>
2022-12-03 09:15:40 +0200

    Solution day 3

--- src/main/kotlin/p03/t1/solution.kt
+++ src/main/kotlin/p03/t1/solution.kt
@@ -1,5 +1,14 @@
 package p03.t1
 
-fun solution(data: List<String>) {
-
+fun solution(data: List<String>): Int {
+    return data.asSequence()
+        .map { line ->
+            val first = line.take(line.length / 2).toSet()
+            val second = line.takeLast(line.length / 2).toSet()
+            val common = first.intersect(second).first()
+            ('a'..'z').indexOf(common).takeIf { it >= 0 }?.let { it + 1 }
+                ?: ('A'..'Z').indexOf(common).takeIf { it >= 0 }?.let { it + 27 }
+                ?: throw Exception("Unknown symbol $common")
+        }
+        .sum()
 }
--- src/test/kotlin/p03/t1/test.kt
+++ src/test/kotlin/p03/t1/test.kt
@@ -3,18 +3,19 @@ package p03.t1
 import DataResolver
 import java.nio.file.Path
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Run {
     private val resolver = DataResolver(dataPath = Path.of("p03", "t1"))
 
     @Test
     fun sample() {
-
+        assertEquals(157, solution(resolver.sample))
     }
 
     @Test
     fun artifact() {
-
+        assertEquals(7691, solution(resolver.artifact))
     }
 }
 
