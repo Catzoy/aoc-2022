337e01e5b8122e88ca0658b8a882409f9b1ddb56
Catzoy <demchenko20021@gmail.com>
2022-12-04 09:03:33 +0200

    Solution day 4

--- src/main/kotlin/p04/t1/solution.kt
+++ src/main/kotlin/p04/t1/solution.kt
@@ -1,5 +1,14 @@
 package p04.t1
 
-fun solution(data: List<String>) {
-
+fun solution(data: List<String>): Int {
+    val rangesRegex = Regex("(\\d+)-(\\d+),(\\d+)-(\\d+)")
+    return data.asSequence()
+        .map { line ->
+            val match = rangesRegex.find(line) ?: throw Exception("Line in wrong format: $line")
+            match.groupValues.asSequence().drop(1).map { it.toInt() }.toList()
+        }.map { (start1, end1, start2, end2) ->
+            start1..end1 to start2..end2
+        }.count { (range1, range2) ->
+            range1.subtract(range2).isEmpty() || range2.subtract(range1).isEmpty()
+        }
 }
